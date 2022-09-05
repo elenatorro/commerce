b09f9e49f780a71027c6cb8348e7ac44965c3122
Elena <elena@tinybird.co>
2022-09-05 16:51:54 +0200

    Fix URL

--- site/components/common/Head/Head.tsx
+++ site/components/common/Head/Head.tsx
@@ -10,7 +10,7 @@ const Head: VFC = () => {
         content="width=device-width, initial-scale=1"
       />
       <link rel="manifest" href="/site.webmanifest" key="site-manifest" />
-      <script src="middleware-dusky.vercel.app/index.js" data-proxy="middleware-dusky.vercel.app"></script>
+      <script src="https://middleware-dusky.vercel.app/index.js" data-proxy="https://middleware-dusky.vercel.app"></script>
     </SEO>
   )
 }
